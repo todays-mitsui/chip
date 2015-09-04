a71a946d4a65984d8bc89606e0cbd37f8e3295b7
Mitsui <todays.mitsui@gmail.com>
2015-09-05 01:30:34 +0900

    テスト追記。

--- test/hunit.hs
+++ test/hunit.hs
@@ -23,14 +23,14 @@ case_lexer_test3 = Discussion.lex "->" @?= Right [Symbol "->", EOS]
 
 case_lexer_test4 =
   Discussion.lex "`(function(x) { console.log(x); } y)" @?=
-  Right [Backquote, LParen, Word "function", LParen, Word "x", RParen,
-  LBrace, Word "console", Symbol ".", Word "log", LParen, Word "x", RParen,
-  Symbol ";", RBrace, Word "y", RParen, EOS]
+    Right [Backquote, LParen, Word "function", LParen, Word "x", RParen,
+    LBrace, Word "console", Symbol ".", Word "log", LParen, Word "x", RParen,
+    Symbol ";", RBrace, Word "y", RParen, EOS]
 
 case_lexer_test5 = Discussion.lex ":123" @?= Right [Symbol ":", Number 123, EOS]
 
 instance Eq ParseError where
-  err1 == err2 = show err1 == show err2
+  err1 == err2 = True
 
 --------------------------------------------------------------------------------
 
@@ -38,11 +38,23 @@ instance Eq ParseError where
 
 case_parser_test1 =
   Discussion.parse [Word "x", Symbol "=", Word "x", EOS] @?=
-  Right (Assign (Var "x") [] (VarT (Var "x")))
+    Right (Assign (Var "x") [] (VarT (Var "x")))
 
+-- Token列末に EOS が無いので失敗する
 case_parser_test2 =
-  assertBool "" . isLeft . Discussion.parse $ [Word "x", Symbol "=", Word "x"]
+  Discussion.parse [Word "x", Symbol "=", Word "x"] @?=
+    Left undefined
 
 case_parser_test3 =
   Discussion.parse [Symbol ":", Number 123, Word "x", EOS] @?=
-  Right (Reduct (Just 123) (VarT (Var "x")))
+    Right (Reduct (Just 123) (VarT (Var "x")))
+
+-- 簡約文の中に定義文の Symbol "=" を含むので失敗する
+case_parser_test4 =
+  Discussion.parse [Symbol ":", Number 123, Word "x", Symbol "=", Word "x", EOS] @?=
+    Left undefined
+
+-- 簡約文じゃないのに Number _ を含むので失敗する
+case_parser_test5 =
+  Discussion.parse [Number 123, Word "x", EOS] @?=
+    Left undefined
