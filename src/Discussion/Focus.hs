a37a3058f6fbc89134ee666b2bd0793faacf712b
Mitsui <todays.mitsui@gmail.com>
2015-10-06 08:20:12 +0900

    Focus型のAppRCrumbの仕様を少しだけ変更。
    Lambda式に与えられた引数の数を数えやすくするためにAppRCrumb自体にListを持たせるようにした。
    途中でJointが割り込んでくると計数が複雑になるので他にもいいやり方あるかも知れないけど。
    
    TODO:
    FocusでTermを辿るとき単式のAppや式を持たないAppに出会うとエラー飛ぶのでパターンマッチを増やす。

--- src/Discussion/Focus.hs
+++ src/Discussion/Focus.hs
@@ -6,17 +6,23 @@ import           Discussion.Data
 
 type Focus       = (Term, Breadcrumbs)
 type Breadcrumbs = [Crumb]
-data Crumb       = AppRCrumb Term
+data Crumb       = AppRCrumb [Term]
                  | AppLCrumb [Term]
                  | LambdaCrumb Args
                  | JointCrumb
-                   deriving (Eq, Show)
+                     deriving (Eq, Show)
 
 --------------------------------------------------------------------------------
 
 goAppL :: Focus -> Maybe Focus
-goAppL (App (t1:t2:[]), bs) = Just (t1           , AppRCrumb t2        : bs)
-goAppL (App ts        , bs) = Just (App (init ts), AppRCrumb (last ts) : bs)
+goAppL (App (t1:t2:[]), bs) =
+  case bs of
+       (AppRCrumb ts' : bs') -> Just (t1 , AppRCrumb (t2:ts') : bs')
+       _                     -> Just (t1 , AppRCrumb [t2]     : bs)
+goAppL (App ts        , bs) =
+  case bs of
+       (AppRCrumb ts' : bs') -> Just (App (init ts), AppRCrumb (last ts:ts') : bs')
+       _                     -> Just (App (init ts), AppRCrumb [last ts]     : bs)
 goAppL _                    = Nothing
 
 goAppR :: Focus -> Maybe Focus
@@ -32,11 +38,13 @@ goIntoJoint (Joint t, bs) = Just (t, JointCrumb : bs)
 goIntoJoint _             = Nothing
 
 goUp :: Focus -> Maybe Focus
-goUp (App ts, AppRCrumb t    : bs) = Just (App (ts ++ [t]), bs)
-goUp (t1    , AppRCrumb t2   : bs) = Just (App [t1, t2]   , bs)
-goUp (t     , AppLCrumb ts   : bs) = Just (App (ts ++ [t]), bs)
-goUp (t     , LambdaCrumb vs : bs) = Just (Lambda vs t    , bs)
-goUp (t     , JointCrumb     : bs) = Just (Joint t        , bs)
+goUp (App ts, AppRCrumb (t :[])  : bs) = Just (App (ts ++ [t]), bs)
+goUp (App ts, AppRCrumb (t :ts') : bs) = Just (App (ts ++ [t]), AppRCrumb ts' : bs)
+goUp (t1    , AppRCrumb (t2:[])  : bs) = Just (App [t1, t2]   , bs)
+goUp (t1    , AppRCrumb (t2:ts') : bs) = Just (App [t1, t2]   , AppRCrumb ts' : bs)
+goUp (t     , AppLCrumb ts       : bs) = Just (App (ts ++ [t]), bs)
+goUp (t     , LambdaCrumb vs     : bs) = Just (Lambda vs t    , bs)
+goUp (t     , JointCrumb         : bs) = Just (Joint t        , bs)
 goUp (_     , [])                  = Nothing
 
 goRoot :: Focus -> Maybe Focus
--- test/UnitFocus.hs
+++ test/UnitFocus.hs
@@ -26,32 +26,32 @@ r = root i
 
 --------------------------------------------------------------------------------
 
-case_1st_go_app_left = goAppL r @?= Just (App [s, k], [AppRCrumb k])
+case_1st_go_app_left = goAppL r @?= Just (App [s, k], [AppRCrumb [k]])
 
 case_then_2nd_go_app_left =
-  goAppL (App [s, k], [AppRCrumb k])
-    @?= Just (s , [AppRCrumb k, AppRCrumb k])
+  goAppL (App [s, k], [AppRCrumb [k]])
+    @?= Just (s , [AppRCrumb [k, k]])
 
 case_then_3rd_go_into_lambda =
-  goIntoLambda (s , [AppRCrumb k, AppRCrumb k])
-    @?= Just (App [VarT x, VarT z, App [VarT y, VarT z]], [LambdaCrumb [x, y, z], AppRCrumb k, AppRCrumb k])
+  goIntoLambda (s , [AppRCrumb [k, k]])
+    @?= Just (App [VarT x, VarT z, App [VarT y, VarT z]], [LambdaCrumb [x, y, z], AppRCrumb [k, k]])
 
 case_then_4th_go_app_right =
-  goAppR (App [VarT x, VarT z, App [VarT y, VarT z]], [LambdaCrumb [x, y, z], AppRCrumb k, AppRCrumb k])
-    @?= Just (App [VarT y, VarT z], [AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb k, AppRCrumb k])
+  goAppR (App [VarT x, VarT z, App [VarT y, VarT z]], [LambdaCrumb [x, y, z], AppRCrumb [k, k]])
+    @?= Just (App [VarT y, VarT z], [AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb [k, k]])
 
 case_then_5th_go_into_joint =
-  goIntoJoint (App [VarT y, VarT z], [AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb k, AppRCrumb k])
+  goIntoJoint (App [VarT y, VarT z], [AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb [k, k]])
     @?= Nothing
 
 case_once_again_5th_go_app_left =
-  goAppL (App [VarT y, VarT z], [AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb k, AppRCrumb k])
-    @?= Just (VarT y, [AppRCrumb (VarT z), AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb k, AppRCrumb k])
+  goAppL (App [VarT y, VarT z], [AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb [k, k]])
+    @?= Just (VarT y, [AppRCrumb [VarT z], AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb [k, k]])
 
 case_then_go_root =
-  goRoot (VarT y, [AppRCrumb (VarT z), AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb k, AppRCrumb k])
+  goRoot (VarT y, [AppRCrumb [VarT z], AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb [k, k]])
     @?= Just r
 
 case_last_get_root =
-  getRoot (VarT y, [AppRCrumb (VarT z), AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb k, AppRCrumb k])
+  getRoot (VarT y, [AppRCrumb [VarT z], AppLCrumb [VarT x, VarT z], LambdaCrumb [x, y, z], AppRCrumb [k, k]])
     @?= i
